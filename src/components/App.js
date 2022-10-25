7d74c8b6180a66aec8905962ae657522b4bc5424
John Harold Rasco <john.harold.rasco@codeandtheory.com>
2022-10-25 10:25:10 +0800

    Configure React app for deployment to GitHub Pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,30 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  Route,
-  Switch
-} from 'react-router-dom';
+import React from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 // App components
-import Header from './Header';
-import Home from './Home';
-import About from './About';
-import Teachers from './Teachers';
-import Courses from './Courses';
-import NotFound from './NotFound';
-import Featured from './Featured';
+import Header from "./Header";
+import Home from "./Home";
+import About from "./About";
+import Teachers from "./Teachers";
+import Courses from "./Courses";
+import NotFound from "./NotFound";
+import Featured from "./Featured";
 
 const App = () => (
-  <BrowserRouter>
+  <HashRouter basename="/course-directory">
     <div className="container">
       <Header />
-      
+
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/about" render={ () => <About title='About' /> } />
+        <Route path="/about" render={() => <About title="About" />} />
         <Route exact path="/teachers" component={Teachers} />
         <Route path="/teachers/:topic/:name" component={Featured} />
         <Route path="/courses" component={Courses} />
         <Route component={NotFound} />
       </Switch>
     </div>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default App;
-
-
-
-
