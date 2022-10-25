9c4157ba482e55773198ba21f0c5c442793b0273
John Harold Rasco <john.harold.rasco@codeandtheory.com>
2022-10-25 10:16:22 +0800

    Updates

---
+++ precache-manifest.95ce1f934948da2c2f723d7c29de7999.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9613c4ea2fa93aaea0f7",
+    "url": "/course-directory/static/css/main.7ffa0eac.chunk.css"
+  },
+  {
+    "revision": "9613c4ea2fa93aaea0f7",
+    "url": "/course-directory/static/js/main.9613c4ea.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "989c10f009fb91cf8611f0892b66deb0",
+    "url": "/course-directory/index.html"
+  }
+];
